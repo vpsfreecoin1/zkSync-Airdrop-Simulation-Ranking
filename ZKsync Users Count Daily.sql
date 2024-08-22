cb9ceaaa782fcf1210a1812fc942cac17c4d124c
vpsfreecoin1 <97170991+vpsfreecoin1@users.noreply.github.com>
2024-08-22 16:41:39 +0700

    Create ZKsync Users Count Daily.sql

---
+++ ZKsync Users Count Daily.sql
@@ -0,0 +1,12 @@
+select date_trunc('day', block_date) as block_date,
+    version,
+    count(*) as transaction_count,
+    count(distinct user_address) as user_count,
+    sum(amount_usd) filter (where amount_usd > 0) as bridged_amount_usd,
+    sum(amount_eth) filter (where amount_eth > 0) as bridged_amount_eth,
+    sum(amount_usd) as balance_amount_usd,
+    sum(amount_eth) as balance_amount_eth
+from query_2501864
+where block_date >= date('2023-03-01')
+group by 1, 2
+order by 1, 2
